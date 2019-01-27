0314cc0a480f33d1dce98f6d97e49c18fdd07b67
GedMarc <ged_marc@hotmail.com>
2019-01-27 10:13:39 +0200

    module-info removal of import statements

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -1,17 +1,3 @@
-import com.jwebmp.core.base.angular.services.IAngularConfiguration;
-import com.jwebmp.core.base.angular.services.IAngularController;
-import com.jwebmp.core.base.angular.services.IAngularModule;
-import com.jwebmp.core.services.IPageConfigurator;
-import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
-import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
-import com.jwebmp.guicedservlets.services.IGuiceSiteBinder;
-import com.jwebmp.plugins.blueimp.fileupload.BlueImpFileUploadBinderGuiceSiteBinder;
-import com.jwebmp.plugins.blueimp.fileupload.BlueImpFileUploadDataBinderConfigurationBase;
-import com.jwebmp.plugins.blueimp.fileupload.BlueImpFileUploadPageConfigurator;
-import com.jwebmp.plugins.blueimp.fileupload.angular.AngularBlueImpFileUploadModule;
-import com.jwebmp.plugins.blueimp.fileupload.angular.BlueImpFileDestroyController;
-import com.jwebmp.plugins.blueimp.fileupload.implementations.BlueImpFileUploadExclusionsModule;
-
 module com.jwebmp.plugins.blueimp.fileupload {
 
 	exports com.jwebmp.plugins.blueimp.fileupload;
@@ -39,14 +25,14 @@ module com.jwebmp.plugins.blueimp.fileupload {
 	requires com.jwebmp.interception;
 	requires com.jwebmp.core.angularjs;
 
-	provides IAngularConfiguration with BlueImpFileUploadDataBinderConfigurationBase;
-	provides IAngularController with BlueImpFileDestroyController;
-	provides IAngularModule with AngularBlueImpFileUploadModule;
-	provides IPageConfigurator with BlueImpFileUploadPageConfigurator;
-	provides IGuiceSiteBinder with BlueImpFileUploadBinderGuiceSiteBinder;
+	provides com.jwebmp.core.base.angular.services.IAngularConfiguration with com.jwebmp.plugins.blueimp.fileupload.BlueImpFileUploadDataBinderConfigurationBase;
+	provides com.jwebmp.core.base.angular.services.IAngularController with com.jwebmp.plugins.blueimp.fileupload.angular.BlueImpFileDestroyController;
+	provides com.jwebmp.core.base.angular.services.IAngularModule with com.jwebmp.plugins.blueimp.fileupload.angular.AngularBlueImpFileUploadModule;
+	provides com.jwebmp.core.services.IPageConfigurator with com.jwebmp.plugins.blueimp.fileupload.BlueImpFileUploadPageConfigurator;
+	provides com.jwebmp.guicedservlets.services.IGuiceSiteBinder with com.jwebmp.plugins.blueimp.fileupload.BlueImpFileUploadBinderGuiceSiteBinder;
 
-	provides IGuiceScanJarExclusions with BlueImpFileUploadExclusionsModule;
-	provides IGuiceScanModuleExclusions with BlueImpFileUploadExclusionsModule;
+	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions with com.jwebmp.plugins.blueimp.fileupload.implementations.BlueImpFileUploadExclusionsModule;
+	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.jwebmp.plugins.blueimp.fileupload.implementations.BlueImpFileUploadExclusionsModule;
 
 	opens com.jwebmp.plugins.blueimp.fileupload to com.fasterxml.jackson.databind, com.jwebmp.core;
 	opens com.jwebmp.plugins.blueimp.fileupload.options to com.fasterxml.jackson.databind, com.jwebmp.core;
